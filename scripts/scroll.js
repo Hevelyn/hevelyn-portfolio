849cd679a6eadf1b742a2781b2331407d75d22b4
Hevelyn <hevelynsoares15@gmail.com>
2022-12-05 15:50:33 -0300

    add project

--- scripts/scroll.js
+++ scripts/scroll.js
@@ -2,8 +2,8 @@ window.sr = ScrollReveal({ reset: true });
 ScrollReveal({ distance: "15px" });
 sr.reveal(".linkedin", { duration: 3000, origin: "left" });
 sr.reveal(".about-me", { duration: 3000, origin: "right" });
-sr.reveal(".projects-items", { duration: 3000, origin: "bottom" });
-sr.reveal(".project", { duration: 3000, origin: "bottom" });
+// sr.reveal(".projects-items", { duration: 3000, origin: "bottom" });
+// sr.reveal(".project", { duration: 3000, origin: "bottom" });
 sr.reveal(".github-link", { duration: 3000, origin: "bottom" });
 sr.reveal(".conteiner-skills", { duration: 3000, origin: "left" });
 sr.reveal(".conteiner-description", { duration: 3000, origin: "right" });
@@ -11,22 +11,22 @@ sr.reveal(".conteiner-description", { duration: 3000, origin: "right" });
 let largura = window.screen.width;
 
 if (largura >= 300 || largura <= 499) {
-  sr.reveal(".projects-items", {
-    delay: 250,
-    duration: 2500,
-    reset: true,
-    origin: "bottom",
-  });
-  sr.reveal(".project", {
-    delay: 250,
-    duration: 2500,
-    reset: true,
-    origin: "bottom",
-  });
-  sr.reveal(".github-link", {
-    delay: 250,
-    duration: 2500,
-    reset: true,
-    origin: "bottom",
-  });
+  // sr.reveal(".projects-items", {
+  //   delay: 250,
+  //   duration: 2500,
+  //   reset: true,
+  //   origin: "bottom",
+  // });
+  // sr.reveal(".project", {
+  //   delay: 250,
+  //   duration: 2500,
+  //   reset: true,
+  //   origin: "bottom",
+  // });
+  // sr.reveal(".github-link", {
+  //   delay: 250,
+  //   duration: 2500,
+  //   reset: true,
+  //   origin: "bottom",
+  // });
 }
